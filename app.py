fa92366a6dda28cdcb79e417574889aa077ff827
Pokkao9 <panomkorn8525@gmail.com>
2026-05-22 16:03:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ st.set_page_config(page_title="MyApp", layout="wide")
 st.title("🏠 หน้าหลัก ")
 st.write("### Boot Camp: Data Science and Machine Learning")
 st.info("7 Day Intensive Hands-on Workshop")
-st.write("Pokao")
+st.markdown(''':rainbow[Pokkao999] ''')
 st.write("##### Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
-
-st.markdown(''':rainbow[Pokkao999j] ''')
 
 if st.button("💰 ระบบคำนวณส่วนลดตามยอดซื้อ"):
     st.switch_page("pages/app1_discount_calc.py")
