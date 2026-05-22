11a5d417a44a5a8313b2e3b437de8b58d46cabfc
Pokkao9 <panomkorn8525@gmail.com>
2026-05-22 16:01:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ st.info("7 Day Intensive Hands-on Workshop")
 st.write("Pokao")
 st.write("##### Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
 
+st.markdown(''':rainbow[Pokkao999j] ''')
+
 if st.button("💰 ระบบคำนวณส่วนลดตามยอดซื้อ"):
     st.switch_page("pages/app1_discount_calc.py")
 elif st.button("การทำความสะอาดข้อมูล By B"):
