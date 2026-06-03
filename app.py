c048ef95de900c828dd415bedeb4259e48651561
Pokkao9 <panomkorn8525@gmail.com>
2026-06-03 11:33:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ elif st.button("การทำความสะอาดข้อมูล By 
     st.switch_page("pages/clean_By_B2_app.py")
 elif st.button("การทำความสะอาดข้อมูล"):
     st.switch_page("pages/clean_app.py")
-elif st.button("การทำแปลงข้อมูล"):
+elif st.button("การแปลงข้อมูล"):
     st.switch_page("pages/transform_app.py")
